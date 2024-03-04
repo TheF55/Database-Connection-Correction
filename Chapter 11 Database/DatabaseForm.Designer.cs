9b609fa3d1fcd9ce4fa64e139a5f3bfd31fe919d
TheF55 <162068579+TheF55@users.noreply.github.com>
2024-03-03 19:03:56 -0600

    Add files via upload

---
+++ Chapter 11 Database/DatabaseForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Chapter_11_Database
+{
+    partial class DatabaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnameTextBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.FixErrorsButton = new System.Windows.Forms.Button();
+            this.idNoLabel = new System.Windows.Forms.Label();
+            this.yearsLabel = new System.Windows.Forms.Label();
+            this.lNameLabel = new System.Windows.Forms.Label();
+            this.fNameLabel = new System.Windows.Forms.Label();
+            this.IdNoTextBox = new System.Windows.Forms.TextBox();
+            this.YearsTextBox = new System.Windows.Forms.TextBox();
+            this.fNameTextBox = new System.Windows.Forms.TextBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.deletedDataGridView = new System.Windows.Forms.DataGridView();
+            this.displayDataGridButton = new System.Windows.Forms.Button();
+            this.restoreButton = new System.Windows.Forms.Button();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.deletedDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lnameTextBox
+            // 
+            this.lnameTextBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.lnameTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.lnameTextBox.Location = new System.Drawing.Point(254, 206);
+            this.lnameTextBox.Name = "lnameTextBox";
+            this.lnameTextBox.Size = new System.Drawing.Size(124, 20);
+            this.lnameTextBox.TabIndex = 37;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.titleLabel.Location = new System.Drawing.Point(212, 69);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(457, 63);
+            this.titleLabel.TabIndex = 38;
+            this.titleLabel.Text = "Database Connection Form";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NextButton
+            // 
+            this.NextButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.NextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.NextButton.Location = new System.Drawing.Point(388, 259);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(86, 31);
+            this.NextButton.TabIndex = 39;
+            this.NextButton.Text = "&Next";
+            this.NextButton.UseVisualStyleBackColor = false;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // FixErrorsButton
+            // 
+            this.FixErrorsButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.FixErrorsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FixErrorsButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FixErrorsButton.Location = new System.Drawing.Point(492, 259);
+            this.FixErrorsButton.Name = "FixErrorsButton";
+            this.FixErrorsButton.Size = new System.Drawing.Size(86, 31);
+            this.FixErrorsButton.TabIndex = 40;
+            this.FixErrorsButton.Text = "Fix Errors";
+            this.FixErrorsButton.UseVisualStyleBackColor = false;
+            this.FixErrorsButton.Click += new System.EventHandler(this.FixErrorsButton_Click);
+            // 
+            // idNoLabel
+            // 
+            this.idNoLabel.AutoSize = true;
+            this.idNoLabel.Location = new System.Drawing.Point(215, 177);
+            this.idNoLabel.Name = "idNoLabel";
+            this.idNoLabel.Size = new System.Drawing.Size(33, 13);
+            this.idNoLabel.TabIndex = 41;
+            this.idNoLabel.Text = "IdNo:";
+            // 
+            // yearsLabel
+            // 
+            this.yearsLabel.AutoSize = true;
+            this.yearsLabel.Location = new System.Drawing.Point(211, 244);
+            this.yearsLabel.Name = "yearsLabel";
+            this.yearsLabel.Size = new System.Drawing.Size(37, 13);
+            this.yearsLabel.TabIndex = 42;
+            this.yearsLabel.Text = "Years:";
+            // 
+            // lNameLabel
+            // 
+            this.lNameLabel.AutoSize = true;
+            this.lNameLabel.Location = new System.Drawing.Point(187, 209);
+            this.lNameLabel.Name = "lNameLabel";
+            this.lNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.lNameLabel.TabIndex = 43;
+            this.lNameLabel.Text = "Last Name:";
+            // 
+            // fNameLabel
+            // 
+            this.fNameLabel.AutoSize = true;
+            this.fNameLabel.Location = new System.Drawing.Point(398, 209);
+            this.fNameLabel.Name = "fNameLabel";
+            this.fNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.fNameLabel.TabIndex = 44;
+            this.fNameLabel.Text = "First Name:";
+            this.fNameLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // IdNoTextBox
+            // 
+            this.IdNoTextBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.IdNoTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.IdNoTextBox.Location = new System.Drawing.Point(254, 174);
+            this.IdNoTextBox.Name = "IdNoTextBox";
+            this.IdNoTextBox.Size = new System.Drawing.Size(64, 20);
+            this.IdNoTextBox.TabIndex = 45;
+            // 
+            // YearsTextBox
+            // 
+            this.YearsTextBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.YearsTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.YearsTextBox.Location = new System.Drawing.Point(254, 241);
+            this.YearsTextBox.Name = "YearsTextBox";
+            this.YearsTextBox.Size = new System.Drawing.Size(64, 20);
+            this.YearsTextBox.TabIndex = 46;
+            // 
+            // fNameTextBox
+            // 
+            this.fNameTextBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.fNameTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.fNameTextBox.Location = new System.Drawing.Point(464, 206);
+            this.fNameTextBox.Name = "fNameTextBox";
+            this.fNameTextBox.Size = new System.Drawing.Size(124, 20);
+            this.fNameTextBox.TabIndex = 47;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.DeleteButton.Location = new System.Drawing.Point(601, 259);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(86, 31);
+            this.DeleteButton.TabIndex = 48;
+            this.DeleteButton.Text = "&Delete";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // deletedDataGridView
+            // 
+            this.deletedDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.deletedDataGridView.Location = new System.Drawing.Point(197, 364);
+            this.deletedDataGridView.Name = "deletedDataGridView";
+            this.deletedDataGridView.Size = new System.Drawing.Size(472, 150);
+            this.deletedDataGridView.TabIndex = 49;
+            this.deletedDataGridView.Visible = false;
+            // 
+            // displayDataGridButton
+            // 
+            this.displayDataGridButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.displayDataGridButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayDataGridButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.displayDataGridButton.Location = new System.Drawing.Point(149, 313);
+            this.displayDataGridButton.Name = "displayDataGridButton";
+            this.displayDataGridButton.Size = new System.Drawing.Size(99, 31);
+            this.displayDataGridButton.TabIndex = 50;
+            this.displayDataGridButton.Text = "Display Grid";
+            this.displayDataGridButton.UseVisualStyleBackColor = false;
+            this.displayDataGridButton.Click += new System.EventHandler(this.displayDataGridButton_Click);
+            // 
+            // restoreButton
+            // 
+            this.restoreButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.restoreButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restoreButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.restoreButton.Location = new System.Drawing.Point(254, 313);
+            this.restoreButton.Name = "restoreButton";
+            this.restoreButton.Size = new System.Drawing.Size(99, 31);
+            this.restoreButton.TabIndex = 51;
+            this.restoreButton.Text = "Restore";
+            this.restoreButton.UseVisualStyleBackColor = false;
+            this.restoreButton.Visible = false;
+            this.restoreButton.Click += new System.EventHandler(this.restoreButton_Click);
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MessageLabel.Location = new System.Drawing.Point(372, 434);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(118, 21);
+            this.MessageLabel.TabIndex = 52;
+            this.MessageLabel.Text = "No Records Found";
+            this.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MessageLabel.Visible = false;
+            // 
+            // DatabaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 526);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.restoreButton);
+            this.Controls.Add(this.displayDataGridButton);
+            this.Controls.Add(this.deletedDataGridView);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.fNameTextBox);
+            this.Controls.Add(this.YearsTextBox);
+            this.Controls.Add(this.IdNoTextBox);
+            this.Controls.Add(this.fNameLabel);
+            this.Controls.Add(this.lNameLabel);
+            this.Controls.Add(this.yearsLabel);
+            this.Controls.Add(this.idNoLabel);
+            this.Controls.Add(this.FixErrorsButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.lnameTextBox);
+            this.Name = "DatabaseForm";
+            this.Text = "Database Connection/ Correction";
+            this.Load += new System.EventHandler(this.DatabaseForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.deletedDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lnameTextBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button FixErrorsButton;
+        private System.Windows.Forms.Label idNoLabel;
+        private System.Windows.Forms.Label yearsLabel;
+        private System.Windows.Forms.Label lNameLabel;
+        private System.Windows.Forms.Label fNameLabel;
+        private System.Windows.Forms.TextBox IdNoTextBox;
+        private System.Windows.Forms.TextBox YearsTextBox;
+        private System.Windows.Forms.TextBox fNameTextBox;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.DataGridView deletedDataGridView;
+        private System.Windows.Forms.Button displayDataGridButton;
+        private System.Windows.Forms.Button restoreButton;
+        private System.Windows.Forms.Label MessageLabel;
+    }
+}
+
---
+++ Chapter 11 Database/DatabaseForm.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Common;
+
+/* Project         Module 9 
+ * Programmer      Theodore Feldman
+ * Date            10/30/2023
+ * Description     Through buttons on screan you can fix errors, delete records, look through those deleted records, and resore deleted records in an Acess database
+ */
+
+namespace Chapter_11_Database
+{
+    public partial class DatabaseForm : Form
+    {
+        string connString = "provider=Microsoft.ACE.OLEDB.12.0; Data Source=BarneyBear.accdb";
+        DataTable dt = new DataTable();
+        int positionInteger = 0;
+        int lengthDataInteger;
+        int indexInteger;
+
+        string originalFNameString;
+        string originalLNameString;
+        string correctedString;
+
+        public DatabaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            positionInteger += 1;
+
+            if(dt.TableName == "Emp_Deleted")
+            {
+                dt.TableName = "Employees";
+                dt.Reset();
+                DatabaseForm_Load(sender, e);
+            }
+
+            if(positionInteger == dt.Rows.Count)
+            {
+                positionInteger= 0;
+            }
+            this.IdNoTextBox.Text = dt.Rows[positionInteger]["IdNo"].ToString();
+            this.lnameTextBox.Text = dt.Rows[positionInteger]["LName"].ToString();
+            this.fNameTextBox.Text = dt.Rows[positionInteger]["FName"].ToString();
+            this.YearsTextBox.Text = dt.Rows[positionInteger]["Years"].ToString();
+
+
+        }
+
+        private void DatabaseForm_Load(object sender, EventArgs e)
+        {
+            dt.TableName = "Employees";
+            string dataSourceString = "SELECT * From employees";
+
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataSourceString, connString);
+
+            dataAdapter.Fill(dt);
+            dataAdapter.Dispose();
+
+            this.IdNoTextBox.Text = dt.Rows[positionInteger]["IdNo"].ToString();
+            this.lnameTextBox.Text= dt.Rows[positionInteger]["LName"].ToString();
+            this.fNameTextBox.Text = dt.Rows[positionInteger]["FName"].ToString();
+            this.YearsTextBox.Text = dt.Rows[positionInteger]["Years"].ToString();
+
+
+
+
+        }
+
+        private void FixErrorsButton_Click(object sender, EventArgs e)
+        {
+            //Corrects data entry fields
+
+            //Variables
+            int recordCountInteger;
+           
+            positionInteger = 0;
+            indexInteger = 0;
+            correctedString = "";
+            recordCountInteger = dt.Rows.Count;
+
+            try
+            {
+                while (positionInteger < recordCountInteger)
+                {
+                    FixLastName();
+                    FixFirstName();
+                    positionInteger++;
+
+                    if (positionInteger < recordCountInteger)
+                    {
+                        this.lnameTextBox.Text = dt.Rows[positionInteger]["LName"].ToString();
+                        this.fNameTextBox.Text = dt.Rows[positionInteger]["FName"].ToString();
+                    }
+
+                }
+                positionInteger--;
+
+                dt.Reset();
+                DatabaseForm_Load(sender, e);
+                positionInteger = 0;
+
+                this.IdNoTextBox.Text = dt.Rows[positionInteger]["IdNo"].ToString();
+                this.lnameTextBox.Text = dt.Rows[positionInteger]["LName"].ToString();
+                this.fNameTextBox.Text = dt.Rows[positionInteger]["FName"].ToString();
+                this.YearsTextBox.Text = dt.Rows[positionInteger]["Years"].ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The error is:" + ex, "Error", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void FixLastName()
+        {
+            indexInteger = 0;
+            correctedString = "";
+            lengthDataInteger = this.lnameTextBox.Text.Length;
+
+            while (indexInteger < lengthDataInteger)
+            {
+                if (this.lnameTextBox.Text.Substring(indexInteger, 1) != "#")
+                {
+                    if (this.lnameTextBox.Text.Substring(indexInteger, 1) != "&")
+                    {
+                        if (this.lnameTextBox.Text.Substring(indexInteger, 1) != "$")
+                        {
+                            if (this.lnameTextBox.Text.Substring(indexInteger, 1) != "*")
+                            {
+                                correctedString += this.lnameTextBox.Text.Substring(indexInteger, 1);
+                            }
+                        }
+                    }
+
+
+                }
+                indexInteger++;
+            }
+
+            //Updates the database
+
+            originalLNameString = this.lnameTextBox.Text.ToString();
+
+            this.lnameTextBox.Text = Convert.ToString(correctedString);
+
+            string dataSourceString = "UPDATE Employees SET LName = '" + this.lnameTextBox.Text + "' WHERE LName = '" + Convert.ToString(originalLNameString) + "'";
+
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataSourceString, connString);
+
+            dt.AcceptChanges();
+            dataAdapter.Fill(dt);
+            dataAdapter.Update(dt);
+        }
+
+        private void FixFirstName()
+        {
+            indexInteger = 0;
+            correctedString = "";
+            lengthDataInteger = this.fNameTextBox.Text.Length;
+
+            while (indexInteger < lengthDataInteger)
+            {
+                if (this.fNameTextBox.Text.Substring(indexInteger, 1) != "#")
+                {
+                    if (this.fNameTextBox.Text.Substring(indexInteger, 1) != "&")
+                    {
+                        if (this.fNameTextBox.Text.Substring(indexInteger, 1) != "$")
+                        {
+                            if (this.fNameTextBox.Text.Substring(indexInteger, 1) != "*")
+                            {
+                                correctedString += this.fNameTextBox.Text.Substring(indexInteger, 1);
+                            }
+                        }
+                    }
+
+
+                }
+                indexInteger++;
+            }
+
+            //Updates the database
+
+            originalFNameString = this.fNameTextBox.Text.ToString();
+
+            this.fNameTextBox.Text = Convert.ToString(correctedString);
+
+            string dataSourceString = "UPDATE Employees SET FName = '" + this.fNameTextBox.Text + "' WHERE FName = '" + Convert.ToString(originalFNameString) + "'";
+
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataSourceString, connString);
+
+            dt.AcceptChanges();
+            dataAdapter.Fill(dt);
+            dataAdapter.Update(dt);
+        }
+
+
+            private void label3_Click(object sender, EventArgs e)
+        {
+            //******************Ignore********************
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            //Moves the current record into the deleted table - Emp_Deleted
+
+            DataTable dtDel = new DataTable();
+
+            try
+            {
+                dtDel.TableName = "Emp_Deleted";
+                connString = "provider=Microsoft.ACE.OLEDB.12.0;Data Source = BarneyBear.accdb";
+
+                string dataInsertString = "INSERT INTO Emp_Deleted (IdNo, LName, FName, Years) VALUES ('" + this.IdNoTextBox.Text + "', '"
+                    + this.lnameTextBox.Text + "', '"
+                    + this.fNameTextBox.Text + "', '"
+                    + this.YearsTextBox.Text + "')";
+
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataInsertString, connString);
+
+                dataAdapter.Fill(dtDel);
+                dataAdapter.Dispose();
+
+                
+
+                dataInsertString = "DELETE * FROM Employees WHERE IdNo = '" + this.IdNoTextBox.Text + "'";
+
+                dataAdapter = new OleDbDataAdapter(dataInsertString, connString);
+                dataAdapter.Fill(dt);
+                dataAdapter.Dispose();
+                positionInteger= 0;
+                dt.Reset();
+                DatabaseForm_Load(sender, e);
+
+                MessageBox.Show("Record Deleted", "Deletion Report", MessageBoxButtons.OK);
+
+
+            }
+            catch (Exception theException) 
+            {
+                MessageBox.Show("Error: " + theException, "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void displayDataGridButton_Click(object sender, EventArgs e)
+        {
+            dt.TableName = "Emp_Deleted";
+
+            int recordCountInteger; 
+
+            connString = "provider=Microsoft.ACE.OLEDB.12.0;Data Source = BarneyBear.accdb";
+            string dataSourceString = "SELECT * FROM Emp_Deleted";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataSourceString, connString);
+
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            dataAdapter.Dispose();
+
+            recordCountInteger = dt.Rows.Count;
+
+            if(recordCountInteger <= 0)
+            {
+                this.MessageLabel.Visible = true;
+                this.deletedDataGridView.Visible= false;
+                this.restoreButton.Visible = false;
+            }
+            else
+            {
+                this.deletedDataGridView.DataSource = dt;
+                this.deletedDataGridView.Visible = true;
+
+                this.restoreButton.Visible = true;
+                MessageLabel.Visible = false;
+            }
+
+            
+        }
+
+        private void restoreButton_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow currentRow = this.deletedDataGridView.CurrentRow;
+
+            this.IdNoTextBox.Text = currentRow.Cells[0].Value.ToString();
+            this.lnameTextBox.Text = currentRow.Cells[1].Value.ToString();
+            this.fNameTextBox.Text = currentRow.Cells[2].Value.ToString();
+            this.YearsTextBox.Text = currentRow.Cells[3].Value.ToString();
+
+            dt.TableName = "Employees";
+            connString = "provider=Microsoft.ACE.OLEDB.12.0;Data Source = BarneyBear.accdb";
+
+            string dataInsertString = "INSERT INTO Employees (IdNo, LName, FName, Years) VALUES ('" + this.IdNoTextBox.Text + "', '"
+                    + this.lnameTextBox.Text + "', '"
+                    + this.fNameTextBox.Text + "', '"
+                    + this.YearsTextBox.Text + "')";
+
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataInsertString, connString);
+
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            dataAdapter.Dispose();
+
+            DeleteFromDeleted();
+
+            this.displayDataGridButton.PerformClick();
+        }
+
+        private void DeleteFromDeleted()
+        {
+            dt.TableName = "Emp_Deleted";
+
+            string dataDeleteString = "DELETE * FROM Emp_Deleted WHERE IdNo = '" + this.IdNoTextBox.Text + "'";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(dataDeleteString, connString);
+
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            dataAdapter.Dispose();
+
+
+        }
+    }
+}
